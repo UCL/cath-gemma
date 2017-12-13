f3dfc10c5f8798111128b3afe65c27b3ac964e82
Tony Lewis <tonyelewis@users.noreply.github.com>
2017-12-13 09:39:09 +0000

    Add scripts for preparing data

---
+++ Cath-Gemma/script/get_sf_seqs_from_gene3d_db.pl
@@ -0,0 +1,113 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+
+# Core
+use Carp    qw/ confess        /;
+use English qw/ -no_match_vars /;
+use FindBin;
+
+use lib "$FindBin::Bin/../extlib/lib/perl5";
+
+# Non-core
+# use DDP colored => 1;
+use DBI;
+use Log::Log4perl::Tiny qw/ :easy /;
+use Path::Tiny;
+
+# Database connection details
+my $dsn        = "dbi:Oracle:host=odb.cs.ucl.ac.uk;sid=cathora1";
+my $user       = "orengoreader";
+my $pass       = "orengoreader";
+my $tablespace = 'gene3d_16';
+
+# Grab command line options
+if ( scalar( @ARGV ) > 2 ) {
+	die "usage: $PROGRAM_NAME <cath_node_id> <file_suffix>\n(writes output files, one per superfamily, in current directory)\n"
+}
+my ( $cath_node_id, $suffix ) = @ARGV;
+$cath_node_id //= '';
+$suffix       //= '.gene3d_16.uniprot_accs__ids__seqs';
+
+# Check the CATH node is valid and convert it into a suitable 'LIKE' string
+if ( $cath_node_id !~ /^(\d+(\.\d+){0,3})?$/ ) {
+	die "Not a valid CATH node : \"$cath_node_id\"\n";
+}
+my $cath_node_id_like_str =
+	$cath_node_id
+	 . ( ( $cath_node_id =~ /^\d+(\.\d+){0,2}$/ ) ? '.' : '' )
+	 . '%';
+
+# Connect to the database
+my $dbh = DBI->connect( $dsn, $user, $pass, { LongReadLen => 100000000 })
+	or die "! Error: failed to connect to database: $OS_ERROR";
+
+# Grab the superfamily IDs within the specified CATH node
+INFO "Getting list of superfamilies within $cath_node_id from the database";
+my $superfamily_ids_sql = <<"_SQL";
+SELECT
+  SUPERFAMILY
+FROM
+  $tablespace.CATH_DOMAIN_PREDICTIONS
+WHERE
+  SUPERFAMILY LIKE ?
+GROUP BY
+  SUPERFAMILY
+ORDER BY
+  SUPERFAMILY
+_SQL
+my $superfamily_ids_sth = $dbh->prepare( $superfamily_ids_sql )
+	or die "! Error: failed to prepare statement: " . $dbh->err;
+$superfamily_ids_sth->execute( $cath_node_id_like_str )
+	or die "! Error: failed to execute statement: " . $superfamily_ids_sth->err;
+my @superfamily_ids;
+while ( my $row = $superfamily_ids_sth->fetchrow_hashref ) {
+	push @superfamily_ids, $row->{ SUPERFAMILY };
+}
+
+
+# Loop over the superfamily IDs
+foreach my $superfamily_id ( @superfamily_ids ) {
+	INFO "Getting information from the database for superfamily \"$superfamily_id\"";
+	# Grab the sequences for the superfamily
+	my $sequences_for_superfamily_sql = <<"_SQL";
+SELECT
+  u.accession                         AS uniprot_acc,
+  c.sequence_md5 || '/' || c.resolved AS domain_id,
+  s.aa_sequence                       AS sequence
+FROM
+  $tablespace.CATH_DOMAIN_PREDICTIONS c
+INNER JOIN
+  $tablespace.SEQUENCES               s ON c.sequence_md5 = s.sequence_md5
+INNER JOIN
+  $tablespace.UNIPROT_PRIM_ACC        u ON c.sequence_md5 = u.sequence_md5
+WHERE
+  c.superfamily = ?
+_SQL
+	my $sequences_for_superfamily_sth = $dbh->prepare( $sequences_for_superfamily_sql )
+		or die "! Error: failed to prepare statement: " . $dbh->err;
+	$sequences_for_superfamily_sth->execute( $superfamily_id )
+		or die "! Error: failed to execute statement: " . $sequences_for_superfamily_sth->err;
+	my @results;
+	while ( my $row = $sequences_for_superfamily_sth->fetchrow_hashref() ) {
+		my $domain_id = $row->{ DOMAIN_ID };
+		$domain_id =~ s/,/_/g; # / Convert commas to underscores
+		push @results, [
+			$row->{ UNIPROT_ACC },
+			$domain_id,
+			$row->{ SEQUENCE    },
+		];
+	}
+
+	# Write the results to a file
+	path( $superfamily_id . $suffix )->spew(
+		join (
+			'',
+			map {
+				join( "\t", @$ARG ) . "\n";
+			} @results
+		)
+	);
+
+}
---
+++ Cath-Gemma/script/get_uniprot_accs_of_md5s.pl
@@ -0,0 +1,166 @@
+#!/usr/bin/env perl
+
+=usage
+
+get_uniprot_accs_of_md5s.pl
+
+Consider using:
+
+setenv DBIC_TRACE console
+
+...or...
+
+export DBIC_TRACE=console
+
+...if you want more info on the DB queries
+
+=cut
+
+use strict;
+use warnings;
+
+# Core
+use Carp                qw/ confess        /;
+use Data::Dumper;
+use English             qw/ -no_match_vars /;
+use feature             qw/ say            /;
+use List::Util          qw/ shuffle        /;
+use FindBin;
+
+use lib "$FindBin::Bin/../extlib/lib/perl5";
+
+# Non-core (local)
+# use DDP colored => 1;
+use List::MoreUtils     qw/ natatime       /;
+use Log::Log4perl::Tiny qw/ :easy          /;
+use Path::Tiny;
+
+# Cath
+use Cath::Schema::Biomap;
+
+# The number of IDs to which everything should be limited
+# (or undef for (no no, no no no no, no no no no, no no there's) no limit)
+my $NUM_IDS;
+# my $NUM_IDS    = 5000;
+
+# The number of IDs per DB query batch
+# (Oracle appears to have a limit of 1000)
+my $BATCH_SIZE = 999;
+
+my $orig_seqs_data = path( '/cath/people2/ucbcdal/dfx_funfam2013_data/shared/used/domseq_data_gene3d_12_31072013/persf/3.30.70.1470.faa' );
+
+if ( ! -s $orig_seqs_data ) {
+	confess 'Argh $orig_seqs_data';
+}
+
+INFO 'Reading IDs from ' . $orig_seqs_data;
+my $orig_seqs_fh = $orig_seqs_data->openr();
+my %seq_of_orig_id;
+my %md5_of_orig_id;
+while ( my $orig_seqs_line = <$orig_seqs_fh> ) {
+	while ( chomp( $orig_seqs_line ) ) {}
+	if ( $orig_seqs_line =~ /^>(\S+)$/ ) {
+		my $id = $1;
+		$orig_seqs_line = <$orig_seqs_fh>;
+		my $seq = $orig_seqs_line;
+		while ( chomp( $seq ) ) {}
+		my @a = ( $id, $seq );
+		my $md5 = $id;
+		$md5 =~ s/\/.*$//g;
+		$md5_of_orig_id{ $id } = $md5;
+		$seq_of_orig_id{ $id } = $seq;
+	}
+	else {
+		confess $orig_seqs_line;
+	}
+}
+$orig_seqs_fh->close();
+
+my @ids = shuffle( values( %md5_of_orig_id ) );
+
+INFO 'Found ' . scalar( @ids ) . ' distinct MD5 ids from ' . scalar( keys ( %md5_of_orig_id ) ) . ' IDs';
+
+
+
+if ( defined( $NUM_IDS ) ) {
+	INFO 'Processing a limit of ' . $NUM_IDS . ' ids';
+	@ids = @ids[ 0 .. $NUM_IDS ];
+}
+
+
+
+INFO 'Connecting to DB';
+my $db = Cath::Schema::Biomap->connect_by_version( 'v4_0' )
+	or confess;
+
+my %results;
+
+my $ids_batch_itr = natatime $BATCH_SIZE, @ids;
+while (my @batch_ids = $ids_batch_itr->() ) {
+	INFO 'Querying DB with ' . scalar( @batch_ids ) . ' ids';
+
+	my @batch_results = map {
+		my %columns = $ARG->get_columns();
+		[ $columns{ sequence_md5 }, $columns{ uniprot_acc } ];
+	} $db->resultset( 'UniprotAccession' )->search(
+		{
+			sequence_md5 => {
+				IN => \@batch_ids,
+			}
+		},
+		{
+			select => [ qw/ sequence_md5 uniprot_acc / ],
+			as     => [ qw/ sequence_md5 uniprot_acc / ],
+		}
+	)->all();
+
+	foreach my $batch_result ( @batch_results ) {
+		push @{ $results{ $batch_result->[ 0 ] } }, $batch_result->[ 1 ];
+	}
+}
+
+foreach my $orig_id ( sort( keys( %md5_of_orig_id ) ) ) {
+	my $md5          = $md5_of_orig_id{ $orig_id };
+	my $uniprot_accs = $results{ $md5_of_orig_id{ $orig_id } };
+	my $seq          = $seq_of_orig_id{ $orig_id };
+	if ( ! defined( $uniprot_accs ) ) {
+		warn 'Did not find any UniProt accessions for ' . $md5;
+		next;
+	}
+	say $uniprot_accs->[ 0 ] . ' ' . $orig_id . ' ' . $seq_of_orig_id{ $orig_id };
+}
+
+=head1 NAME
+
+make_starting_clusters.pl - Make sequences files for clusters with GO annotations
+
+=head1 SYNOPSIS
+
+perl -I extlib/lib/perl5 script/make_starting_clusters.pl [options]
+
+=head1 OPTIONS
+
+    -h [ --help ]                       Output usage message
+
+    -c [ --cluster-infile ] <file>      Read cluster membership from file <file>
+    -i [ --ids-go-terms-infile ] <file> Read IDs and GO terms from file <file>
+    -s [ --sequences-infile ] <file>    Read sequences from file <file>
+
+    -o [ --membership-outdir ] <dir>    Output help message
+
+    -g [ --excluded-go-terms ] <list>   Only consider GO terms beginning IEA: if they're in comma-separated list <list>
+                                        Default: IEA:UniProtKB-KW,IEA:UniProtKB-EC
+=head1 DESCRIPTION
+
+Make the starting clusters for GeMMA to process as part of the FunFam protocol.
+
+This identifies the clusters that have any (non-excluded) GO annotations
+and writes out the sequence files for them.
+
+All GO terms beginning with N are excluded.
+
+To understand how to use this script, see:
+
+See https://github.com/UCL/cath-gemma/wiki/Running-the-Full-FunFam-Protocol
+
+=cut
