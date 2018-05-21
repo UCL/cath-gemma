95d69972bd8b280ce097ea8344e9ed2fd2e884f8
sillitoe <ian@sillit.com>
2018-05-21 14:19:27 +0100

    add hhsearch scan parser

--- Cath-Gemma/lib/Cath/Gemma/Scan/ScanData.pm
+++ Cath-Gemma/lib/Cath/Gemma/Scan/ScanData.pm
@@ -148,4 +148,74 @@ sub parse_from_raw_compass_scan_output_lines {
 	);
 }
 
+
+=head2 parse_from_raw_hhsearch_scan_output_lines
+
+TODOCUMENT
+
+=cut
+
+sub parse_from_raw_hhsearch_scan_output_lines {
+	state $check = compile( Invocant, ArrayRef[Str], Int );
+	my ( $proto, $output_lines, $expected_num_results ) = $check->( @ARG );
+
+	my %best_score_per_match;
+	my $alignment_suffix = alignment_suffix();
+
+	# Query         1
+	# Match_columns 52
+	# No_of_seqs    1 out of 2
+	# Neff          1
+	# Searched_HMMs 2
+	# Date          Fri May 18 18:52:41 2018
+	# Command       /tmp/cath_gemma_exes_dir.0nFqYd81/hhsearch -i /cath/homes2/ucbcisi/git/cath-gemma/Cath-Gemma/t/data/1.20.5.200/profiles/1.hhconsensus.a3m -d /tmp/cath-gemma-util.FxeNsNpH/.n0de_37693cfc748049e45d87b8c7d8b9aacd.KG2eHKHjEJF.db 
+
+	#  No Hit                             Prob E-value P-value  Score    SS Cols Query HMM  Template HMM
+	#   1 2                               98.5 3.5E-15 1.7E-15   69.1   0.0   49    2-52      2-50  (50)
+	#   2 3                               97.4 2.6E-11 1.3E-11   52.0   0.0   46    5-52      1-46  (46)
+	#   3 3                                0.0       2    0.98    4.2   0.0    1    1-1       1-1   (46)
+	#   4 2                                0.0       2       1    2.2   0.0    1    6-6       1-1   (50)
+
+	my $line_count=0;
+	my $total_lines = scalar @$output_lines;
+	my $query_id;
+	while ( $line_count < $total_lines ) {
+		my $line = $output_lines->[ $line_count++ ];
+		if ( $line =~ /^Query\s+(\S+)/ ) {
+			$query_id = $1;
+		}
+		last if $line =~ /^\s+No\s+Hit/;	
+	}
+	while ( $line_count < $total_lines ) {
+		my $line = $output_lines->[ $line_count++ ];
+		$line =~ s/^\s+//;
+		next unless $line;
+		my @cols = split( /\s+/, $line );
+
+		confess "expected 11 columns, got $#cols columns (line: $line_count): '$line'"
+			if scalar @cols != 11;
+
+		my ($num, $match_id, $prob, $evalue, $pvalue, $score, $ss, $cols, $query_hmm, $template_hmm, $something)
+			= @cols;
+		
+		my $data = [ $query_id, $match_id, $evalue ];
+		$best_score_per_match{$match_id} //= $data;
+
+		if ( $evalue < $best_score_per_match{$match_id}->[2] ) {
+			$best_score_per_match{$match_id} = $data;
+		}
+	}
+
+	my $num_results = scalar keys %best_score_per_match;
+
+	if ( $num_results != $expected_num_results ) {
+		confess "Something wrong whilst parsing HHSuite results: expected to get $expected_num_results results but found $num_results."
+	}
+
+	# sort by lowest evalue
+	my @outputs = sort { $a->[2] <=> $b->[2] } values %best_score_per_match; 
+	return __PACKAGE__->new(
+		scan_data => \@outputs
+	);
+}
 1;
