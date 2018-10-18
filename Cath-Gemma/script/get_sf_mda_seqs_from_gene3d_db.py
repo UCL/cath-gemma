c6c4437557f7b2f115617230c9476ce408a469b1
sillitoe <ian@sillit.com>
2018-10-18 21:27:30 +0100

    print domain sequences (not protein sequences)

--- Cath-Gemma/script/get_sf_mda_seqs_from_gene3d_db.py
+++ Cath-Gemma/script/get_sf_mda_seqs_from_gene3d_db.py
@@ -5,6 +5,7 @@ import argparse
 import logging
 import os
 import sys
+import re
 
 # include
 import cx_Oracle
@@ -27,8 +28,6 @@ parser.add_argument('--evalue', '-e', type=str, default='0.001', dest='max_evalu
 parser.add_argument('--verbose', '-v', required=False, action='count', default=0,
     help='more verbose logging')
 
-
-
 class Segment(object):
     def __init__(self, start, stop):
         self.start = start
@@ -36,29 +35,29 @@ class Segment(object):
     
     @classmethod
     def new_from_string(cls, segstr):
-        start, stop = segstr.split('-')
+        try:
+            start, stop = segstr.split('-')
+        except:
+            raise Exception("failed to parse segment '{}'".format(segstr))
+
         return cls(start, stop)
 
 class Domain(object):
-    def __init__(self, id, sfam_id, *, md5=None, mda=None, segments=None):
+    def __init__(self, id, sfam_id, *, segments):
         self.id = id
         self.sfam_id = sfam_id
-        self.md5 = md5
-        self.mda = mda
-        self.start = None
+        self.segments = segments
+        self.start = segments[0].start
 
-        if segments:
-            if isinstance(segments, str):
-                segs = []
-                for segstr in segments.split(','):
-                    seg = Segment.new_from_string(segstr)
-                    segs.append(seg)
-                segments = segs
-            self.segments = segments
-            self.start = segments[0].start
+    @property
+    def segment_info(self):
+        return ",".join(['{}-{}'.format(s.start, s.stop) for s in self.segments])
+
+    def __str__(self):
+        return "{}".format(self.id)
 
 class Protein(object):
-    def __init__(self, id, *, seq=None):
+    def __init__(self, id, seq=None):
         self.id = id
         self.seq = seq
         self.domains = {}
@@ -66,6 +65,12 @@ class Protein(object):
     def to_mda_string(self):
         domains = sorted(self.domains.values(), key=lambda dom: dom.start)
         return '-'.join([d.sfam_id for d in domains])
+
+    def __str__(self):
+        domains = sorted(self.domains.values(), key=lambda dom: dom.start)
+        desc = "Protein: {} (seq len:{})".format(self.id, len(self.seq) if self.seq else 'None')
+        desc += "\n".join(["  {:<40} [{}]".format(d, d.sfam_id) for d in domains])
+        return desc
 
 class GenerateMdaSequences(object):
     """
@@ -80,17 +85,6 @@ class GenerateMdaSequences(object):
         self.sfam_id = sfam_id
         self.uniprot_chunk_size = uniprot_chunk_size
         self._proteins = {}
-
-    def merge_proteins(self, proteins):
-        for p in proteins.values():
-            domains_to_merge = p.domains
-            if p.id in self._proteins:
-                p = self._proteins[p.id]
-            else:
-                self._proteins[p.id] = p
-            
-            for d in domains_to_merge.values():
-                p.domains[d.id] = d
 
     def run(self):
 
@@ -135,6 +129,42 @@ class GenerateMdaSequences(object):
             # merge them back in
             self.merge_proteins(proteins)
 
+    def merge_proteins(self, proteins):
+        for p in proteins.values():
+            domains_to_merge = p.domains
+            if p.id in self._proteins:
+                p = self._proteins[p.id]
+            else:
+                self._proteins[p.id] = p
+            
+            for d in domains_to_merge.values():
+                if d.id not in p.domains: # make sure we don't overwrite domains with sequences
+                    p.domains[d.id] = d
+
+    def get_chopped_sequence(self, full_sequence, segments):
+        domain_seq = ''
+        domain_length = sum([(int(s.stop) - int(s.start) + 1) for s in segments])
+
+        # aln_seq = '-' * len(full_sequence)
+        for seg in segments:
+            domain_seq += full_sequence[int(seg.start)-1:int(seg.stop)]
+            # aln_seq = '{}{}{}'.format(aln_seq[:int(seg.start)-1], domain_seq, aln_seq[int(seg.stop):])
+
+        if domain_length != len(domain_seq):
+            raise Exception( "expected domain length {} from segment info {}, actually got {} '{}'".format(
+                domain_length, 
+                ','.join(['{}-{}'.format(s.start, s.stop) for s in segments]), 
+                len(domain_seq), domain_seq
+            ))
+
+        # chopping_str = ",".join(["{}-{}".format(s.start,s.stop) for s in segments])
+        # logger.debug( "SEGMENTS: {}".format(chopping_str) )
+        # logger.debug( "PROTEIN:  {}".format(full_sequence) )
+        # logger.debug( "DOMAIN:   {}".format(aln_seq) )
+
+        return domain_seq
+
+
     def get_mda_summary(self):
         """Returns a dict containing the number of occurences of each MDA."""
         mda_count = {}
@@ -155,19 +185,23 @@ class GenerateMdaSequences(object):
 
         with open(out_filename, 'w') as fout:
 
-            # print out the proteins
+            # print out the domains in this superfamily
             for p in self._proteins.values():
                 try:
                     mda = p.to_mda_string()
                 except:
                     logger.error("failed to generate mda string for protein: {}".format(p.id))
                     raise
-                seq = p.seq
-                for d in p.domains.values():
-                    fout.write("{}\n".format( "\t".join([ p.id, d.id, mda, seq ]) ) )
+                
+                sfam_domains = [d for d in p.domains.values() if d.sfam_id == self.sfam_id]
 
-    def _segs_from_string(self, segs_str):
-        return [(seg.split('-')) for seg in segs_str.split(',')]
+                for d in sfam_domains:
+                    try:
+                        dom_seq = self.get_chopped_sequence(p.seq, d.segments)
+                    except:
+                        raise Exception("failed to chop segments {} from protein {}".format(d, p))
+                    
+                    fout.write("{}\n".format( "\t".join([ p.id, d.id, mda, dom_seq]) ) )
 
     def get_proteins_for_uniprot_ids(self, uniprot_ids):
         
@@ -182,12 +216,15 @@ class GenerateMdaSequences(object):
         cur.execute(sequences_for_uniprot_sql, uniprot_ids)
         for result in cur:
             uniprot_acc, md5, sfam_id, resolved = (result)
+            resolved = resolved.replace(',', '_')
             domain_id = '{}/{}'.format(md5, resolved)
-            # segs = self._segs_from_string(resolved)
+            segs = self._segs_from_string(resolved)
             # dom = { "id": domain_id, "sfam_id": sfam_id, "segments": segs }
-            dom = Domain(id=domain_id, sfam_id=sfam_id, segments=resolved)
             p = proteins[uniprot_acc]
-            p.domains[domain_id] = dom
+            dom = Domain(id=domain_id, sfam_id=sfam_id, segments=segs)
+
+            if domain_id not in p.domains: # do not overwrite existing domains (ie with sequence data)
+                p.domains[domain_id] = dom
 
         return proteins
 
@@ -202,8 +239,19 @@ class GenerateMdaSequences(object):
         proteins_extra = self._get_proteins_for_sfam_sql(sequences_extra_for_superfamily_sql, sfam_id=sfam_id)
 
         proteins.update(proteins_extra)
-    
+
         return proteins
+
+    def _segs_from_string(self, segments_string):
+
+        re_split_segs = re.compile(r'[,_]')
+
+        segs = []
+        for segstr in re_split_segs.split(segments_string):
+            seg = Segment.new_from_string(segstr)
+            segs.append(seg)
+
+        return segs
 
     def _get_proteins_for_sfam_sql(self, sql, *, sfam_id):
 
@@ -217,13 +265,11 @@ class GenerateMdaSequences(object):
         cur.execute(None, { 'sfam_id': sfam_id })
         for result in cur:
             uniprot_acc, md5, seq, resolved = (result)
-            domain_id = '{}/{}'.format(md5, resolved)
             seq = seq.read()
-
+            resolved = resolved.replace(',', '_')
             domain_id = '{}/{}'.format(md5, resolved)
-            # segs = self._segs_from_string(resolved)
+
             # dom = { "id": domain_id, "sfam_id": sfam_id, "segments": segs }
-            dom = Domain(id=domain_id, sfam_id=sfam_id, segments=resolved)
 
             if uniprot_acc in proteins:
                 p = proteins[uniprot_acc]
@@ -231,13 +277,16 @@ class GenerateMdaSequences(object):
                 p = Protein(uniprot_acc, seq=seq)
                 proteins[uniprot_acc] = p
 
+            segs = self._segs_from_string(resolved)
+            dom = Domain(id=domain_id, sfam_id=sfam_id, segments=segs)
+
             p.domains[domain_id] = dom
 
             record_count += 1
             if record_count % 1000 == 0:
                 logger.info("   ... processed {} domain records".format(record_count))
 
-            # print( "{:<10s} {:<10s} {:<10s} {:<10s}".format(uniprot_acc, md5, resolved, seq) )
+            # logger.debug( "{:<10s} {:<10s} {:<10s} {}".format(uniprot_acc, md5, resolved, dom_seq) )
 
         logger.info(" ... got {} unique proteins".format(len(proteins)))
 
